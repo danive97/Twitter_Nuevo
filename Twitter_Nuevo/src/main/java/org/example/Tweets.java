c71adea7891114e2470985e152534f05d16a95e3
Dani <daniel.ojeda@telefonica.net>
2022-06-29 14:57:40 +0200

    Creacion de la clase tweets

---
+++ Twitter_Nuevo/src/main/java/org/example/Tweets.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Tweets {
+
+    private Collection<Tweet> tweets;
+
+    public Tweets() {
+        this.tweets = new ArrayList<>();
+    }
+
+
+    public Collection<Tweet> getTweets() {
+        return tweets;
+    }
+
+    public boolean addTweets(Tweet nuevo) {
+        tweets.add(nuevo);
+        return true;
+    }
+
+}
